e1f0bfd33a311bd8361769f39587deaa35e74b05
shivani <pshivanisharma1@gmail.com>
2022-03-19 14:11:12 +0000

    dd

---
+++ date.php
@@ -0,0 +1,11 @@
+<form action="code.php" method="POST" ; <div class "form-group mb-3" ; <label for="">Name</label>
+    <input type="text" name="name" class="form-control" />
+    </div>
+    <div class="form-group mb-3>
+   <label for=""Date of Birth</label>
+   <input type=" date" name="dateofbirth" class="form control" />
+    </div>
+    <div class="form group mb-3">
+        <button type="submit" name="Save_date" class="btn btn-primary" Save Data</button>
+    </div>
+</form>
